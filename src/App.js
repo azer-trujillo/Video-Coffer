5065ed08161a80b60b433261f58538e2089c5330
Daniel Blumberg <blumdani@isu.edu>
2019-11-21 16:09:52 -0700

    Updated app.js to provide one basic "time" value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import Routes from './routes';
-import './styles/app.css';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Routes from "./routes";
+import "./styles/app.css";
+import moment from "moment";
 
-const App = (props) => {
+const App = props => {
+  const now = moment().format();
   return (
     <div className="App container">
+      <div id="dateTime" align="right">
+        {now}
+      </div>
       <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Video Coffer</Navbar.Brand>
-          <Navbar.Toggle/>
+        <Navbar.Brand href="/">Video Coffer</Navbar.Brand>
+        <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: Anonymous
-          </Navbar.Text>
+          <Navbar.Text>Signed in as: Anonymous</Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
       <Routes />
