820e81490895a61dc39d7708907909a2a876161e
Antonio <danklordswaggins@gmail.com>
2020-01-22 20:02:45 -0600

    Created a Back button & added background for VideoPlayer
    - Back button redirects user to Home screen

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import '../styles/videoPlayer.css';
 
 const VideoPlayer = () => {
@@ -7,9 +9,19 @@ const VideoPlayer = () => {
     const videoId = useSelector(state => state.videoId);
     const { id } = videoId;
 
+    let history = useHistory();
+
+    const handleClick = () => {
+        history.push('/home');
+    }
+
     return (
         <div className="container">
             <div className="mediaPlayer">
+                <Button
+                    variant="primary"
+                    onClick={handleClick}
+                >Go Back</Button>
                 <iframe
                     src={`https://www.youtube.com/embed/${id}`}
                     frameBorder='0'
