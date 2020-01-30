75be44e35e75f04dcc6b70538d85e3e303123a62
Antonio <Trujillo>
2020-01-30 15:02:12 -0600

    Implemented responsiveness for VideoPlayer iframe

--- src/actions/videos.js
+++ src/actions/videos.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const videoSearch = (e) => ({
     type: 'FETCH_VIDEOS',
-    payload: axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${e.keywords}&key=AIzaSyDXLWlXz5NW8PIKVLh7VFpHrjz0Ox7OpqU&pageToken=${e.pageToken}&maxResults=${e.maxResults}&type=video`)
+    payload: axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${e.keywords}&key=AIzaSyAk52PX3rY_f6yNW60SIGY7fS5t9hgdHkw&pageToken=${e.pageToken}&maxResults=${e.maxResults}&type=video`)
 });
 
 export default videoSearch;
