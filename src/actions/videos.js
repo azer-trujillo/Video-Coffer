2cf4e0e10db761ad7b88bc50a3d37575f63b366f
Daniel Blumberg <blumdani@isu.edu>
2020-01-30 15:45:51 -0700

    WatchLater structure plus UI updates

--- src/actions/videos.js
+++ src/actions/videos.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const videoSearch = (e) => ({
-    type: 'FETCH_VIDEOS',
-    payload: axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${e.keywords}&key=AIzaSyDXLWlXz5NW8PIKVLh7VFpHrjz0Ox7OpqU&pageToken=${e.pageToken}&maxResults=${e.maxResults}&type=video`)
+const videoSearch = e => ({
+  type: "FETCH_VIDEOS",
+  payload: axios.get(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${e.keywords}&key=AIzaSyAk52PX3rY_f6yNW60SIGY7fS5t9hgdHkw&pageToken=${e.pageToken}&maxResults=${e.maxResults}&type=video`
+  )
 });
 
 export default videoSearch;
