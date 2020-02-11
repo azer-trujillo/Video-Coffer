13f18804060cc86b9218567abcf1894f085c6ee4
Raul_Lopez <raul_lopez@unitec.edu>
2020-02-11 13:47:59 -0600

    files

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  const store = configureStore();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+      <App />
+      </Router>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
